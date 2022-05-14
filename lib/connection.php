c2d9dd258a4852da8d445669fc64f8e4b1c5e94f
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-14 12:34:17 -0400

    Instalação

---
+++ lib/connection.php
@@ -0,0 +1,23 @@
+<?php
+if ($_SERVER['HTTP_HOST'] === 'localhost' || $_SERVER['HTTP_HOST'] === '10.0.0.115') {
+    define('DB_HOST', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_DATABASE', 'politica');
+} else {
+    define('DB_HOST', '3.93.20.163');
+    define('DB_USERNAME', 'eleitores');
+    define('DB_PASSWORD', 'Mf6t1y76');
+    define('DB_DATABASE', 'eleitores');
+}
+
+
+$con = mysql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD);
+mysql_set_charset('utf8', $con);
+mysql_select_db(DB_DATABASE, $con);
+
+if (!$con) {
+    die('Não foi possível conectar: ' . mysql_error());
+}
+
+#mysql_close($con);
