bc25659c341ab40dc07909b29f574adb2af6906c
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-14 14:47:53 -0400

    Atualizações

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if (!isset($_SESSION['usuario'])) {
 
     <title>Painel - Dashboard</title>
 
-    <?php include "../lib/header.php"; ?>
+    <?php include "./lib/header.php"; ?>
 
     <link
             href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
