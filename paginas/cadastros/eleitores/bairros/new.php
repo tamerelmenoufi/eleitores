73ecef93daacbfb7790e1cdc3a6243ff11dbd412
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-14 22:38:58 -0400

    Atualizações

--- paginas/cadastros/eleitores/bairros/new.php
+++ paginas/cadastros/eleitores/bairros/new.php
@@ -4,9 +4,35 @@
             <h5>Inserir um novo Bairro</h5>
         </div>
         <div class="col-12">
-
+            <div class="form-group">
+                <label for="NovoBairro">Bairro</label>
+                <input type="text" class="form-control" id="NovoBairro" >
+            </div>
+            <button SalvarBairro type="button" class="btn btn-primary">Salvar</button>
         </div>
-
     </div>
-
 </div>
+<script>
+    $(function(){
+        $("button[SalvarBairro]").click(function(){
+            cod='<?=$_GET['codigo']?>';
+            municipio='<?=$_GET['municipio']?>';
+            bairro=$("#NovoBairro").val();
+            if(bairro){
+                $.ajax({
+                    url:"paginas/cadastros/eleitores/bairros/new.php",
+                    type:"POST",
+                    data:{
+                        cod,
+                        municipio,
+                        bairro
+                    },
+                    success:function(dados){
+                        JanelaAddBairro.close();
+                    }
+                });
+            }
+
+        });
+    })
+</script>
