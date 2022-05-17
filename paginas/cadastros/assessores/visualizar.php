802418d40e68e63d942df71f1d9dc11c63fba63d
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-17 11:02:06 -0400

    Atualizações

--- paginas/cadastros/assessores/visualizar.php
+++ paginas/cadastros/assessores/visualizar.php
@@ -28,9 +28,6 @@ $d = mysql_fetch_object($result);
             Visualizar
         </h6>
         <div class="d-md-flex justify-content-xl-center">
-            <?php
-            if(in_array('Assessores - Cadastrar', $ConfPermissoes)){
-            ?>
             <button
                     type="button"
                     class="btn btn-success btn-sm float-left"
@@ -39,10 +36,6 @@ $d = mysql_fetch_object($result);
             >
                 <i class="fa-solid fa-plus"></i> Novo
             </button>
-            <?php
-            }
-            if(in_array('Assessores - Editar', $ConfPermissoes)){
-            ?>
             <button
                     type="button"
                     class="btn btn-warning btn-sm float-left"
@@ -51,10 +44,6 @@ $d = mysql_fetch_object($result);
             >
                 <i class="fa-solid fa-pencil"></i> Editar
             </button>
-            <?php
-            }
-            if(in_array('Assessores - Excluir', $ConfPermissoes)){
-            ?>
             <button
                     type="button"
                     class="btn btn-danger btn-excluir btn-sm float-left"
@@ -62,9 +51,6 @@ $d = mysql_fetch_object($result);
             >
                 <i class="fa-regular fa-trash-can"></i> Excluir
             </button>
-            <?php
-            }
-            ?>
         </div>
     </div>
     <div class="card-body">
