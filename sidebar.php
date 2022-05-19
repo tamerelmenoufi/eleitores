c3da0dabe076c56a22a057ca78de2ee9010f2d0d
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-18 23:14:48 -0400

    Atualizações

--- sidebar.php
+++ sidebar.php
@@ -32,7 +32,7 @@
     <li class="nav-item active">
         <a class="nav-link" href="#" url="paginas/cadastros/eleitores/index.php">
             <i class="fa-solid fa-address-book"></i>
-            <span>Eleitores</span></a>
+            <span>Eleitores x</span></a>
     </li>
 
     <li class="nav-item">
