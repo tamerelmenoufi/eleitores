a1bdd2258284b1e796a44c0a41f9cba1b4477b8c
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-18 23:30:13 -0400

    Atualizações

--- content.php
+++ content.php
@@ -11,15 +11,15 @@ include_once './lib/includes.php';
 <div class="row">
 
     <!-- Pending Requests Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
+    <div class="col-xl-6 col-md-6 mb-4">
         <div class="card border-left-warning shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                            Usuários
+                            Assessores
                         </div>
-                        <div class="h5 mb-0 font-weight-bold text-gray-800">Nome dos Usuários</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">520</div>
                     </div>
                     <div class="col-auto">
                         <i class="fa-solid fa-users fa-2x text-gray-300"></i>
@@ -30,16 +30,16 @@ include_once './lib/includes.php';
     </div>
 
     <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
+    <div class="col-xl-6 col-md-6 mb-4">
         <div class="card border-left-info shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Assessores
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Eleitores
                         </div>
                         <div class="row no-gutters align-items-center">
                             <div class="col-auto">
-                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">Nome dos Assessores</div>
+                                <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">1248</div>
                             </div>
                         </div>
                     </div>
@@ -52,7 +52,7 @@ include_once './lib/includes.php';
     </div>
 
     <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
+    <!-- <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-primary shadow h-100 py-2">
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
@@ -70,7 +70,6 @@ include_once './lib/includes.php';
         </div>
     </div>
 
-    <!-- Earnings (Monthly) Card Example -->
     <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-success shadow h-100 py-2">
             <div class="card-body">
@@ -87,7 +86,7 @@ include_once './lib/includes.php';
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 
 </div>
 
