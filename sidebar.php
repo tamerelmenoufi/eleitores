5213f8382d0958097a7cb9bb1673bb24f174a872
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-19 08:41:11 -0400

    Atualizações

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,8 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+            <!-- <i class="fas fa-laugh-wink"></i> -->
+            <i class="fa-solid fa-ball-pile"></i>
         </div>
         <div class="sidebar-brand-text mx-3" title="Sistema de Registro de Eleitores">SRE</div>
     </a>
