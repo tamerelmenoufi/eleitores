a6860ed6c5a9afdaf66831cbe895ef094216e4cc
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-18 23:37:49 -0400

    Atualizações

--- content.php
+++ content.php
@@ -111,10 +111,10 @@ include_once './lib/includes.php';
                          aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <h4 class="small font-weight-bold">Registro de Eleitores <span
-                            class="float-right">40%</span></h4>
+                            class="float-right">60%</span></h4>
                 <div class="progress mb-4">
                     <div class="progress-bar bg-warning" role="progressbar" style="width: 40%"
-                         aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
+                         aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
                 <!-- <h4 class="small font-weight-bold">Customer Database <span
                             class="float-right">60%</span></h4>
