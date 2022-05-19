d5682c4264bc584407324983583ef15dfdc4f424
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-18 23:36:40 -0400

    Atualizações

--- content.php
+++ content.php
@@ -94,29 +94,29 @@ include_once './lib/includes.php';
 <div class="row">
 
     <!-- Content Column -->
-    <div class="col-lg-6 mb-4">
+    <div class="col-lg-12 mb-4">
 
         <!-- Project Card Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Solicitações de Serviços</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Resumo de Registros</h6>
             </div>
             <div class="card-body">
 
 
-                <h4 class="small font-weight-bold">Server Migration <span
+                <h4 class="small font-weight-bold">Registro de Assessores <span
                             class="float-right">20%</span></h4>
                 <div class="progress mb-4">
                     <div class="progress-bar bg-danger" role="progressbar" style="width: 20%"
                          aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <h4 class="small font-weight-bold">Sales Tracking <span
+                <h4 class="small font-weight-bold">Registro de Eleitores <span
                             class="float-right">40%</span></h4>
                 <div class="progress mb-4">
                     <div class="progress-bar bg-warning" role="progressbar" style="width: 40%"
                          aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
                 </div>
-                <h4 class="small font-weight-bold">Customer Database <span
+                <!-- <h4 class="small font-weight-bold">Customer Database <span
                             class="float-right">60%</span></h4>
                 <div class="progress mb-4">
                     <div class="progress-bar" role="progressbar" style="width: 60%"
@@ -133,7 +133,7 @@ include_once './lib/includes.php';
                 <div class="progress">
                     <div class="progress-bar bg-success" role="progressbar" style="width: 100%"
                          aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
+                </div> -->
             </div>
         </div>
 
