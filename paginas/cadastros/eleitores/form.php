b51f8519022d5c7d209e07e9705a85655d088177
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-20 09:59:35 -0400

    Atualizações

--- paginas/cadastros/eleitores/form.php
+++ paginas/cadastros/eleitores/form.php
@@ -77,7 +77,7 @@ if ($codigo) {
 <div class="card shadow mb-4">
     <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
         <h6 class="m-0 font-weight-bold text-primary">
-            <?= $codigo ? 'Alterar' : 'Cadastrar'; ?> Eleitores
+            <?= $codigo ? 'Alterar' : 'Novo'; ?> Cadastro
         </h6>
     </div>
     <div class="card-body">
