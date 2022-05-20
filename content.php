8d19e6decf44fb4490a673caa8c7d16c31479a4a
tamerelmenoufi <tamer@mohatron.com.br>
2022-05-20 10:02:41 -0400

    Atualizações

--- content.php
+++ content.php
@@ -35,7 +35,7 @@ include_once './lib/includes.php';
             <div class="card-body">
                 <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Cadstros
+                        <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Cadastros
                         </div>
                         <div class="row no-gutters align-items-center">
                             <div class="col-auto">
