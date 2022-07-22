59c9bea285708b86764a410d84d09c308117c52b
tamerelmenoufi <tamer@mohatron.com.br>
2022-07-22 10:10:18 -0400

    instalação

--- paginas/relatorios/graficos/download/bairros.php
+++ paginas/relatorios/graficos/download/bairros.php
@@ -6,10 +6,7 @@ header('Content-Disposition: attachment; filename=bairros.csv');
 
 $md5 = md5(date("YmdHis") . $_SERVER["PHP_SELF"]);
 
-$query = "SELECT bai.descricao AS descricao, COUNT(*) AS qt FROM bairros bai "
-    . "INNER JOIN beneficiados b ON b.bairro = bai.codigo "
-    . "INNER JOIN servicos s ON s.beneficiado = b.codigo "
-    . "GROUP BY bai.descricao ORDER BY qt DESC";
+$query = "SELECT bai.descricao AS descricao, COUNT(*) AS qt FROM bairros bai INNER JOIN eleitores b ON b.bairro = bai.codigo GROUP BY bai.descricao ORDER BY qt DESC";
 
 /*$query = "SELECT b.tipo, COUNT(*) AS qt FROM servicos a "
     . "LEFT JOIN servico_tipo b ON a.tipo = b.codigo GROUP BY a.tipo"
